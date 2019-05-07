40d56d54118e28716264c117f597353ecf697870
Permyakov A. E <permyakov.a.e@yandex.ru>
2019-05-07 19:57:57 +0500

    Шаблон базового примера

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+import customH1 from './components/customH1';
+import axios from 'axios';
+
+export default {
+  name: 'app',
+  components: {
+    customH1,
+  },
+  data(){
+    return{
+      body: '<h1>Загрузка данных</h1>',
+    }
+  },
+  render: function (createElement) {
+    return createElement(
+        'div',
+        {
+          domProps: {
+            innerHTML: this.body
+          }
+        }
+    )
+  },
+  mounted() {
+    let self = this;
+    axios.get('/article.html')
+        .then(function (response) {
+          self.body = response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/customH1.vue
@@ -0,0 +1,11 @@
+<template>
+	<button class="btn badge-primary">
+		<slot></slot>
+	</button>
+</template>
+
+<script>
+	export default {
+		name: "customH1"
+	}
+</script>
